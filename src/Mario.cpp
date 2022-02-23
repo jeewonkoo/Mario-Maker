e98ee1e2d6ec2a027ea6edf50e2ef229033ba5d4
renren123123 <87758256+renren123123@users.noreply.github.com>
2022-02-23 13:14:07 -0600

    added mario cons and update

--- src/Mario.cpp
+++ src/Mario.cpp
@@ -2,12 +2,31 @@
 #include "Mario.h"
 #include <raymath.h>
 
+Mario::Mario(double px, double py) {
+	positionx = px;
+	positiony = py;
+}
 
 void render() {
 
 }
 
 
-void update(bool left, bool right, bool up, bool down) {
-
+void Mario::update(bool left, bool right, bool up, bool down) {
+	if (left == true and right == true) {
+		positionx += 0;
+	}
+	else if (right == true) {
+		positionx += 5;
+	}
+	else if (left == true) {
+		positionx -= 5;
+	}
+	else if (up == true) {
+		positiony += 5;
+	}
+	else if (down == true) {
+		positiony -= 5;
+	}
+	
 }
--- src/Mario.h
+++ src/Mario.h
@@ -5,6 +5,10 @@
 class Mario {
 public:
 	Mario() {};
+	Mario(double px, double py);
 	void render();
 	void update(bool left, bool right, bool up, bool down);
+private:
+	double positionx;
+	double positiony;
 };
