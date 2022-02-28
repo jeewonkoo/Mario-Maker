17c1e342bb5e2687d69ebc7a41a163bce834236c
Nicholas Phillips <nwp2@illinois.edu>
2022-02-28 15:33:52 -0600

    fix syntax in villan

--- src/Villain.cpp
+++ src/Villain.cpp
@@ -5,13 +5,12 @@
 #include <algorithm>
 
 Villain::Villain() {
+}
 
-    void Villain::render(Vector2 top_left, Vector2 size) {
+void Villain::render(Vector2 top_left, Vector2 size) {
 
-    }
+}
 
-    void Villain::update(const Level& level, bool left, bool right, bool up, bool down) {
+void Villain::update(const Level& level, bool left, bool right, bool up, bool down) {
 
-    }
-    
 }
