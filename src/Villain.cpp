c19c60b7042e06410c65265245bb2de17c0eec12
Jeewon Koo <jeewonkoo@wirelessprv-10-195-233-60.near.illinois.edu>
2022-02-28 15:48:50 -0600

    villain class

--- src/Villain.cpp
+++ src/Villain.cpp
@@ -4,13 +4,22 @@
 #include <raylib.h>
 #include <algorithm>
 
-Villain::Villain() {
-}
+Villain::Villain(float px, float py): position({px, py}), velocity({0,0}){}
 
 void Villain::render(Vector2 top_left, Vector2 size) {
 
 }
 
 void Villain::update(const Level& level, bool left, bool right, bool up, bool down) {
+    if (left && right)  velocity.x += 0;
+
+    else if (left)  velocity.x -=0.05; 
+
+    else if (right) velocity.x += 0.05;
+
+    else if (up) velocity.y -= 0.05;
+
+    else if (down) velocity.x += 0.05;
+
 
 }
