d80c6d4877cf8bac3d13f7d60a68db918d6b4527
Elaine Cai <ca.elaine.cai@gmail.com>
2022-02-28 15:08:38 -0600

    attempting to render Mario

--- src/Mario.cpp
+++ src/Mario.cpp
@@ -1,4 +1,4 @@
-
+#include <raylib.h>
 #include "Mario.h"
 #include <raymath.h>
 #include <algorithm>
@@ -6,8 +6,13 @@
 Mario::Mario(float px, float py): position({px, py}), velocity({0,0}){}
 
 void Mario::render(Vector2 top_left, Vector2 size) {
-	DrawRectangleV(Vector2Add(top_left, Vector2Multiply(position, {64.f, 64.f})), mario_size, GREEN);
+	//DrawRectangleV(Vector2Add(top_left, Vector2Multiply(position, {64.f, 64.f})), mario_size, GREEN);
 
+	Image mario_img = LoadImage("images/marios.png");
+	Texture2D mario_texture = LoadTextureFromImage(mario_img);
+	UnloadImage(mario_img);
+
+	DrawTextureV(mario_texture, Vector2Add(top_left, Vector2Multiply(position, { 64.f, 64.f })), WHITE);
 }
 
 void Mario::update(const Level &level, bool left, bool right, bool up, bool down) {
