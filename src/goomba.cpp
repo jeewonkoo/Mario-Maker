cb31ed32f4bf3e26a3e70a344c287b6a6eefba01
renren123123 <87758256+renren123123@users.noreply.github.com>
2022-03-01 15:06:17 -0600

    goomba

--- src/goomba.cpp
+++ src/goomba.cpp
@@ -1,6 +1,6 @@
 #include "Goomba.h"
 
-Goomba::Goomba (float px, float py, Texture texture) : position({ px, py }), velocity({ 0.05,0 }), tex(texture), initialPosition({ px,py }) {}
+Goomba::Goomba(float px, float py, Texture texture) : position({ px, py }), velocity({ 0.05,0 }), tex(texture), initialPosition({ px,py }) {}
 
 void Goomba::render(Vector2 top_left, Vector2 size) {
     DrawTexturePro(tex, Rectangle{ 0, 0, 24, 25 }, Rectangle{ 0, 0, 64, 64 }, Vector2Subtract(top_left, Vector2Multiply(position, { 64.f, 64.f })), 0, WHITE);
