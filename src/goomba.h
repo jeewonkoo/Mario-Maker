b2b302b080dfe01be67fff2c0d1ca5fe65fe3eb0
Jeewon Koo <jeewonkoo@wirelessprv-10-195-233-60.near.illinois.edu>
2022-03-01 14:47:29 -0600

    Goomba class

--- src/goomba.h
+++ src/goomba.h
@@ -1,11 +1,14 @@
 #include "Villain.h"
 
 class Goomba : public Villain {
-    public:
+public:
+
+    Goomba();
+
     void render(Vector2 top_left, Vector2 size, float a, float b, float c, float d);
 
     void update(const Level& level);
 
-    private:
+private:
     
 };
--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include "Level.h"
 #include "Mario.h"
 #include "Villain.h"
+#include "Goomba.h"
 #include <cmath>
 int main(){
     // Initialization
@@ -41,7 +42,7 @@ int main(){
 
 
     Mario mario(10,10, mario_texture);
-    Villain goomba(10, 10, goomba_texture);
+    Goomba goomba(10, 10, goomba_texture);
 
     // Main game loop
     while (!WindowShouldClose()){
@@ -58,17 +59,8 @@ int main(){
         mario.update(level, left, right, up, down, space);
         Camera2D cam{};
         cam.rotation = 0;
-<<<<<<< HEAD
-<<<<<<< HEAD
-        cam.offset = {0,0};
-=======
-        cam.offset = {mario.rect().x * -64 + 512,0};
->>>>>>> 293a1c9a2e87cd3110f8e192bb5341eec388333f
-=======
 
         cam.offset = {mario.rect().x * -64 + 512,0};
-
->>>>>>> 274bb40f83056a77268959f2acca4e277960fe7e
         cam.target = {0,0};
         cam.zoom = 1.0;
 
