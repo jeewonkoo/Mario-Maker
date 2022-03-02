c3a3e01855b546054effff9cd7c23e7537eceb57
Jeewon Koo <jeewonkoo@wirelessprv-10-195-233-60.near.illinois.edu>
2022-03-02 12:52:03 -0600

    merge

--- src/goomba.h
+++ src/goomba.h
@@ -1,23 +1,9 @@
-#ifndef MARIO_MAKER_GOOMBA_H
-#define MARIO_MAKER_GOOMBA_H
 
-#include "Villain.h"
+#include "Villian.h";
 
-class Goomba : public Villain {
-public:
 
-    Goomba(float px, float py, Texture texture);
+class Goomma : class Villian { 
+    public:
 
-    void render(Vector2 top_left, Vector2 size);
-
-    void update(const Level& level);
-
-private:
-    Texture tex;
-    Vector2 position;
-    Vector2 velocity;
-
-    Vector2 initialPosition;
-};
-
-#endif
+    private: 
+}
