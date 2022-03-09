549af0ec095af59edfc7cec5bd8ea1e344edb85c
Jeewon Koo <jeewonkoo@wirelessprv-10-194-68-26.near.illinois.edu>
2022-03-09 12:45:19 -0600

    comments for tilegrid and mario

--- src/TileGrid.h
+++ src/TileGrid.h
@@ -12,31 +12,30 @@
 #include"Tile.h"
 
 class TileGrid {
-public:
-    TileGrid(Texture texture, size_t width, size_t height) {
-        resize(width, height);
-        tex = texture;
+    public:
+        TileGrid(Texture texture, size_t width, size_t height) {
+            resize(width, height);
+            tex = texture;
 
-        for(size_t x = 0; x < width; x++){
-//            at_mut(x, height - 1).solid = true;
-            for(size_t y = 0; y < height; y++){
-                at_mut(x, height - 1 - y).solid = rand() % (y+1) == 0;
+            for(size_t x = 0; x < width; x++){
+    //            at_mut(x, height - 1).solid = true;
+                for(size_t y = 0; y < height; y++){
+                    at_mut(x, height - 1 - y).solid = rand() % (y+1) == 0;
+                }
             }
-        }
-    };
+        };
 
-    void render(Vector2 top_left, Vector2 bottom_right) const;
-    Tile at(int x, int y) const;
-    Tile & at_mut(int x, int y);
-    //collides rectangle against the level geometry
-    TileCollisionSet collide(Rectangle rect) const;
-private:
-    void resize(size_t width, size_t height);
-    static std::optional<Collision> collide_grid(Rectangle rect, int x, int y) ;
-    size_t width;
-    size_t height;
-    std::vector<Tile> tiles{};
-    Texture tex;
+        void render(Vector2 top_left, Vector2 bottom_right) const;
+        Tile at(int x, int y) const;
+        Tile & at_mut(int x, int y);
+        TileCollisionSet collide(Rectangle rect) const;
+    private:
+        void resize(size_t width, size_t height);
+        static std::optional<Collision> collide_grid(Rectangle rect, int x, int y) ;
+        size_t width;
+        size_t height;
+        std::vector<Tile> tiles{};
+        Texture tex;
 };
 
 
